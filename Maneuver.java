3cac14b8d68fbf349f21dd9e0f7bb616f1bbf868
math-rad <59669511+math-rad@users.noreply.github.com>
2023-11-09 17:47:38 -0600

    some

--- Maneuver.java
+++ Maneuver.java
@@ -7,11 +7,12 @@ package frc.robot.commands;
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import java.util.function.Supplier;
+import edu.wpi.first.math.geometry.Translation2d;
 
 public class Maneuver extends CommandBase {
   private final Drivetrain m_drivetrain;
   private final Supplier<Double> m_xaxisSpeedSupplier;
-  private final Supplier<Double> m_zaxisRotateSupplier;
+  private final Supplier<Double> m_yaxisRotateSupplier;
 
   /**
    * Creates a new ArcadeDrive. This command will drive your robot according to the speed supplier
@@ -19,15 +20,15 @@ public class Maneuver extends CommandBase {
    *
    * @param drivetrain The drivetrain subsystem on which this command will run
    * @param xaxisSpeedSupplier Lambda supplier of forward/backward speed
-   * @param zaxisRotateSupplier Lambda supplier of rotational speed
+   * @param yaxisRotateSupplier Lambda supplier of rotational speed
    */
   public Maneuver(
       Drivetrain drivetrain,
       Supplier<Double> xaxisSpeedSupplier,
-      Supplier<Double> zaxisRotateSupplier) {
+      Supplier<Double> yaxisRotateSupplier) {
     m_drivetrain = drivetrain;
     m_xaxisSpeedSupplier = xaxisSpeedSupplier;
-    m_zaxisRotateSupplier = zaxisRotateSupplier;
+    m_yaxisRotateSupplier = yaxisRotateSupplier;
     addRequirements(drivetrain);
   }
 
@@ -38,11 +39,13 @@ public class Maneuver extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_drivetrain.arcadeDrive(m_xaxisSpeedSupplier.get(), m_zaxisRotateSupplier.get());
-  }
-
-  private boolean inDesiredDirection() {
-    return m_drivetrain.getGyroAngleZ() == m_zaxisRotateSupplier.get();
+    var pt = new Translation2d(m_xaxisSpeedSupplier.get(), m_yaxisRotateSupplier.get());
+    double speed;
+    // if (pt.getAngle().getDegrees() == m_drivetrain.getGyroAngleZ()) {
+      
+    // }
+    speed = pt.getNorm();
+    m_drivetrain.arcadeDrive(speed, 0);
   }
 
   // Called once the command ends or is interrupted.
@@ -52,6 +55,6 @@ public class Maneuver extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
-  }
+    return false; // This is a default command 
+  } 
 }
