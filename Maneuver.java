b300ee58ce1b548e43e80eee09c57d2eba6ed0d0
math-rad <59669511+math-rad@users.noreply.github.com>
2023-11-10 09:49:21 -0600

    11/10/2023

--- Maneuver.java
+++ Maneuver.java
@@ -6,6 +6,9 @@ package frc.robot.commands;
 
 import frc.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import java.util.function.Supplier;
 import edu.wpi.first.math.geometry.Translation2d;
 
@@ -13,6 +16,8 @@ public class Maneuver extends CommandBase {
   private final Drivetrain m_drivetrain;
   private final Supplier<Double> m_xaxisSpeedSupplier;
   private final Supplier<Double> m_yaxisRotateSupplier;
+  private final Trigger m_leftButton;
+  private final CommandXboxController control;
 
   /**
    * Creates a new ArcadeDrive. This command will drive your robot according to the speed supplier
@@ -21,14 +26,20 @@ public class Maneuver extends CommandBase {
    * @param drivetrain The drivetrain subsystem on which this command will run
    * @param xaxisSpeedSupplier Lambda supplier of forward/backward speed
    * @param yaxisRotateSupplier Lambda supplier of rotational speed
+   * @param leftButton Left joystick down 
    */
   public Maneuver(
       Drivetrain drivetrain,
       Supplier<Double> xaxisSpeedSupplier,
-      Supplier<Double> yaxisRotateSupplier) {
+      Supplier<Double> yaxisRotateSupplier,
+      Trigger leftButton,
+      CommandXboxController commandXbox) {
     m_drivetrain = drivetrain;
     m_xaxisSpeedSupplier = xaxisSpeedSupplier;
     m_yaxisRotateSupplier = yaxisRotateSupplier;
+    m_leftButton = leftButton;
+    control = commandXbox;
+    //m_downSupplier = downSupplier;
     addRequirements(drivetrain);
   }
 
@@ -39,12 +50,15 @@ public class Maneuver extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    var pt = new Translation2d(m_xaxisSpeedSupplier.get(), m_yaxisRotateSupplier.get());
+    //System.out.print(control.hashCode());
+    double x = m_xaxisSpeedSupplier.get();
+    double y = m_yaxisRotateSupplier.get();
+    var pt = new Translation2d(x, y);
     double speed;
-    // if (pt.getAngle().getDegrees() == m_drivetrain.getGyroAngleZ()) {
-      
-    // }
-    speed = pt.getNorm();
+    double angle;
+    
+    speed = pt.getNorm() * (y >= 0 ? -.5 : .5);
+    //speed = pt.getNorm();
     m_drivetrain.arcadeDrive(speed, 0);
   }
 
